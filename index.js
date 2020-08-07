b1ce2797f3363b6fafe8aa2197812bae15bbab6d
ambrish <ambrish.java@gmail.com>
2020-08-07 13:56:55 +0530

    waitTime increased to make sure all components load properly

--- index.js
+++ index.js
@@ -9,15 +9,14 @@ app.get('/', async (req, res, ) => {
   // res.set({ 'Content-Type': 'text/html', 'Content-Length': resp.length })
   res.send(resp);
 });
-
 async function getPdf() {
     const browser = await puppeteer.launch({
-      headless: true,
-      args: ['--start-maximized'],
-      defaultViewport: null
+        headless: true,
+        args: ['--start-maximized'],
+        defaultViewport: null
     });
     const page = await browser.newPage();
-    // page.setViewport({width: , height: 768});
+    //page.setViewport({width: , height: 768});
     await page.goto('http://10.78.103.231:8080/blueplanet-core-showcase/', {waitUntil: 'networkidle0'});
     console.log('URL Loaded - ' +'http://10.78.103.231:8080/blueplanet-core-showcase/');
     await page.focus('input.fc-username-value');
@@ -30,22 +29,21 @@ async function getPdf() {
     await page.goto('http://10.78.103.231:8080/blueplanet-core-showcase/page/fc-manage-dashboards-demo/%7B%22fcIcon%22:%22ui-icon-dashboard%22,%22fcHeader%22:%22Manage%20Dashboard%22%7D', {waitUntil: 'networkidle0'});
     const treeItem1 = await page.$('#dashboardTree > p-tree > div > div > ul > p-treenode:nth-child(1) > li > div > span.ui-tree-toggler.pi.ui-unselectable-text.ng-star-inserted.pi-caret-right');
     await treeItem1.click();
-    await page.waitFor(5000);
+    await page.waitFor(10000);
     const dashboard = await page.$('#dashboardTree > p-tree > div > div > ul > p-treenode:nth-child(1) > li > ul > p-treenode:nth-child(4) > li > div > span > span > span > span > i');
     dashboard.click();
-    await page.waitFor(5000);
+    await page.waitFor(20000);
     await page.addStyleTag({
-      content: `
+        content: `
            #ui-tabpanel-0 > div {
                  height: 100% !important;
-                 
            }
            #ui-tabpanel-0 > div > fc-manage-dashboards-demo > fc-documentation > div > div:nth-child(4) > div:nth-child(1) {
              height: 2000px !important;
            }
       `
     });
-    await page.waitFor(5000);
+    await page.emulateMediaType('screen');
     const html = await page.content()
     // await page.setContent(html);
     const fcDashBoardDiv = await page.$('fc-dashboard');
@@ -54,13 +52,12 @@ async function getPdf() {
         document.body.innerHTML = ``;
         document.body.appendChild(dashboardNode);
     });
-    page.emulateMediaType('screen');
-    const pdf = await page.pdf({format: 'Letter', printBackground: true, headerTemplate: `<h1>Dashboard Report</h1>`}); // serialized HTML of page DOM.
+    await page.waitFor(20000);
+    const pdf = await page.pdf({format: 'Letter', printBackground: true ,headerTemplate: `<h1>Dashboard Report</h1>`}); // serialized HTML of page DOM.
     await browser.close();
     console.log('pdf generated');
     return pdf;
 }
-
 app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+    console.log('Example app listening on port 8000!')
 });
